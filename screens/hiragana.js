1d5a8dca921d90220b7a0942481082bab2b0fda7
EidorianAvi <reneavila1993@gmail.com>
2020-10-21 15:03:38 -0600

    Create globalStyles and import it into all screens

--- screens/hiragana.js
+++ screens/hiragana.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
+import { globalStyles } from '../styles/global';
 
 export default function Hiragana() {
 
     return (
-        <View style={styles.container}>
+        <View style={globalStyles.container}>
             <Text>Hiragana Screen</Text>
         </View>
     )
 
 }
 
-const styles = StyleSheet.create({
-    container: {
-        padding: 24,
-    }
-});
---
+++ styles/global.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+
+
+export const globalStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+    },
+    titleText: {
+        fontFamily: 'lato-bold',
+        fontSize: 18,
+        color: '#333',
+    },
+    paragraph: {
+        marginVertical: 9,
+        lineHeight: 20,
+    }
+});
